fcc9e36c659dd1154e0386638ca820d9e6de0296
Eero Anttila <eero.anttila@reaktor.fi>
2014-12-15 11:02:09 +0200

    Add index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    DateFormat: require('./src/DateFormat'),
+    DateLocale: require('./src/DateLocale'),
+    DateParse: require('./src/DateParse'),
+    DateRange: require('./src/DateRange'),
+    DateTime: require('./src/DateTime'),
+    Duration: require('./src/Duration')
+}
+
