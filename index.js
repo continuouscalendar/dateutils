1838dd83586f31060baeefca7da1979141f61998
Eero Anttila <eero.anttila@reaktor.fi>
2014-12-15 13:36:13 +0200

    Add release script and move index.js under src

--- index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    DateFormat: require('./src/DateFormat'),
-    DateLocale: require('./src/DateLocale'),
-    DateParse: require('./src/DateParse'),
-    DateRange: require('./src/DateRange'),
-    DateTime: require('./src/DateTime'),
-    Duration: require('./src/Duration')
-}
-
---
+++ release.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+current_dir=$(pwd)
+cd $(dirname $0)
+old_version=$(git tag | tail -n1)
+version=$1
+npm test
+update_version() { sed  -i '' -E "s/(\"version\".*:.*\").*(\".*)/\1$version\2/g" $@; }
+update_version src/index.js
+npm install
+#TODO create js documentation
+#/usr/local/share/npm/lib/node_modules/doxx/bin/doxx --source ./src/main --target ./docs
+if [ "$version" = "" ]
+then
+	echo "Version information not found. Type ./release.sh <version>"
+	echo "Previous version was $old_version"
+else
+	echo "Creating version $version"
+	update_version package.json
+	echo -e "h3. $version \n\n$PLEASE EDIT:\n$(git log --pretty=format:%s $old_version^..)\n\n$(cat History.textile)" > History.textile
+	vim History.textile
+	git add -A .
+	git commit -m "Build for version $version"
+	git tag $version
+	git status
+	echo "Now type:"
+	echo "git push "
+	echo "git push --tags"
+	echo "npm publish"
+fi
+cd $current_dir
+
+
+
