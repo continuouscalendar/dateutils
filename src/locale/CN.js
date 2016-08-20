3ca1ad2d8e0b88d62a95b93104746542e7c7ee53
secondriver <secondriver@yeah.net>
2016-08-20 14:17:47 +0800

    1. Add Chinese festival 2. DateTime add holiday method

--- src/locale/CN.js
+++ src/locale/CN.js
@@ -2,7 +2,7 @@
 var DateTime = require('../DateTime');
 var DateFormat = require('../DateFormat');
 module.exports = {
-    id: 'AU',
+    id: 'CN',
     monthNames: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
     dayNames: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
     shortDayNames: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
@@ -25,5 +25,17 @@ module.exports = {
     weekDateFormat: 'Y年m月d日 D',
     dateTimeFormat: 'Y年m月d日 H时i分s秒',
     firstWeekday: DateTime.MONDAY,
-    holidays: {}
+    holidays: {
+        "01-01": "元旦",
+        "03-08": "妇女节",
+        "03-12": "植树节",
+        "05-01": "劳动节",
+        "05-04": "青年节",
+        "05-12": "护士节",
+        "06-01": "儿童节",
+        "08-01": "建军节",
+        "09-10": "教师节",
+        "10-01": "国庆节",
+        "11-08": "中国记者节"
+    }
 };
