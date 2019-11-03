f0a92a2684991b9a3e58a1c286d235cdafe3ec98
Eero Anttila <eea@iki.fi>
2019-11-03 19:33:46 +0200

    Build for version v0.3.0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "version":  "0.3.1",
+  "version":  "v0.3.0",
   DateFormat: require('./DateFormat'),
   DateLocale: require('./DateLocale'),
   DateParse:  require('./DateParse'),
